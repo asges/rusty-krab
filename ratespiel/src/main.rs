4ee7752923acf1629e9fd0a47bf755298525c8ac
Ashley Gessler <68016255+asges@users.noreply.github.com>
2020-07-15 18:25:33 +0200

    Initial commit.

--- ratespiel/src/main.rs
+++ ratespiel/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to 'Guess the Number'!");
+
+    println!("Please input a number.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+    .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
