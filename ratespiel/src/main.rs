c4a0a542ce1472d84d9c39563b4a13296c97797e
Ashley Gessler <68016255+asges@users.noreply.github.com>
2020-07-17 12:37:32 +0200

    Allows user multiple tries and ends if they win.

--- ratespiel/src/main.rs
+++ ratespiel/src/main.rs
@@ -1,5 +1,5 @@
 use std::io;
-use std:comp::Ordering;
+use std::cmp::Ordering;
 use rand::Rng; //trait that defines methods which random number generators implement
 
 fn main() {
@@ -8,23 +8,28 @@ fn main() {
     //Specify a number between 1 and 100 because as of 15/7/2020, std lib doesn't have a
     // random number generator yet. Library crate exists though
     let secret_number = rand::thread_rng().gen_range(1,101);
+        println!("The secret number is : {}", secret_number);
 
-    println!("The secret number is : {}", secret_number);
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-    .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse()   
+            .expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!");
-        Ordering::Greater => println!("TOo big!");
-        Ordering::Equal => println!("You win!");
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => { 
+                println!("You win!");
+                break;
+            }
+        }
     }
-    //Allowing for multiple guesses with looping
-
-    //Exiting after correct guess
 }
